b66b307fe172ed913850e0ea67e6a2cd883a0bb9
Sam Baldwin <sam.baldwin.05@gmail.com>
2016-02-22 09:22:33 -0500

    Add docker shell script invocation

---
+++ bash/docker_snowmelt_dss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Set enviroments for cron invocation.
+export PATH=/usr/lib64/qt-3.3/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:$PATH
+
+export LD_LIBRARY_PATH=/usr/lib64:/usr/lib:/usr/local/lib:/usr/lib/oracle/11.2/client64:/u01/product/oracle/ofm/11g/Oracle_WT1/lib:$LD_LIBRARY_PATH
+
+# Create DSS files for the watersheds.
+/usr/bin/docker exec -it --user u4rrcsgb snowmelt python /fire/study/snow/nohrsc_gdal/snowmelt_app/snowmelt/scripts/process_extents.py --all --scp
